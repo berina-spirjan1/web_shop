d68eb508e04f59fbac5517443c5d3f9f6233e921
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-12-22 00:49:45 +0100

    Added new crud functionalities.

---
+++ routes/shop_category.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const pg = require("pg");
+const alert = require("alert");
+
+const config = {
+    user: 'vhaxxure',
+    database: 'vhaxxure',
+    password: 'PRTQj-BsWP_lwQCZdqJH94vbpZHUkuAx',
+    host: 'tai.db.elephantsql.com',
+    port: 5432,
+    max: 100,
+    idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+
+let database={
+    getAllCategoriesForShops:function(req,res,next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query("select * from kategorija order by id_kategorije",function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.niz_kategorija = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getAllNumberOfShops:function(req,res,next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select count(*)
+                          from trgovina t, kategorija k
+                          where k.id_kategorije = t.id_kategorije
+                          group by k.id_kategorije
+                          order by k.id_kategorije`,function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.broj_prodavnica = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+}
+
+
+router.get('/', database.getAllCategoriesForShops,
+                     database.getAllNumberOfShops,
+    function(req, res, next) {
+        res.render('crud_for_shop_category',{categories: req.niz_kategorija, number_of_shops: req.broj_prodavnica});
+    });
+
+
+router.get('/delete_shop_category',
+    function(req, res, next) {
+        res.redirect('/home/shop_category');
+    });
+
+
+router.post('/delete_user_type/:id', function(req, res, next) {
+    pool.connect(function (err, client, don) {
+        if (err)
+            throw(err);
+        else {
+            client.query(`delete from kategorija 
+                          where id_kategorije = $1`, [req.params.id], function (err, result) {
+                don();
+                if (err)
+                    throw(err);
+                else{
+                    alert('Successfully deleted shop category!');
+                    res.redirect('/home/shop_category');
+                }
+            });
+        }
+    });
+});
+
+
+router.get('/delete_all',
+    function(req, res, next) {
+        res.redirect('/home/shop_category');
+    });
+
+router.post('/delete_all', function(req, res, next) {
+    pool.connect(function (err, client, don) {
+        if (err)
+            throw(err);
+        else {
+            client.query(`delete from kategorija`, function (err, result) {
+                don();
+                if (err)
+                    throw(err);
+                else{
+                    alert('Successfully deleted all shop categories!');
+                    res.redirect('/home/shop_category');
+                }
+            });
+        }
+    });
+});
+
+
+module.exports = router;
