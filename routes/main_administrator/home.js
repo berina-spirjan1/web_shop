bd3a3240e41ce0e3f1ea60abed89397ca857b37e
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-02 21:37:34 +0100

    Added function for statistics.

--- routes/main_administrator/home.js
+++ routes/main_administrator/home.js
@@ -205,6 +205,74 @@ let database = {
                 }
             });
         });
+    },
+    getMostPopularCategoryOfArticleForSingleDays: function(req, res, next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select max(y.count) as najpopularniji_po_danu, y.naziv_kategorije, y.datum_narudzbe
+                          from (select ka.naziv_kategorije, count(aka.id_kategorija_artikla), n.datum_narudzbe
+                                from kategorija_artikla ka, artikal_kategorija_artikla aka, narudzba n, artikal_narudzba an,
+                                artikal a, korpa k, artikal_trgovina atv
+                                where ka.id_kategorija_artikla = aka.id_kategorija_artikla
+                                and an.id_narudzbe = n.id_narudzbe
+                                and k.id_korpe = an.id_korpe
+                                and at.id_artikla = a.id_artikla
+                                and at.id = an.id_artikal_trgovina
+                                and aka.id_artikla = a.id_artikla
+                                group by ka.naziv_kategorije, n.datum_narudzbe
+                                order by n.datum_narudzbe) y
+                        group by y.naziv_kategorije, y.datum_narudzbe
+                        order by y.datum_narudzbe`,
+                function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.niz_najpopularnijih_kategorija = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getNumberOfOrdersByDay: function (req, res, next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select  datum_narudzbe, count(id_narudzbe)
+                          from narudzba
+                          group by datum_narudzbe
+                          order by datum_narudzbe`,function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.niz_narudzbi_po_danu = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getTotalProfitForSingleDay: function(req, res, next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select  n.datum_narudzbe, sum(a.cijena_artikla * at.kolicina) as zarada
+                          from artikal a, artikal_narudzba an, artikal_trgovina at, narudzba n
+                          where a.id_artikla = at.id_artikla
+                          and an.id_artikal_trgovina = at.id
+                          and n.id_narudzbe = an.id_narudzbe
+                          group by n.datum_narudzbe
+                          order by n.datum_narudzbe`,function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.profit_po_danu = result.rows;
+                    next();
+                }
+            });
+        });
     }
 }
 
@@ -260,8 +328,17 @@ router.get('/customer', function(req, res, next) {
     res.render('index',{title:'GOTOVOOO3'});
 });
 
-router.get('/statistics',database.getNumberOfOrdersForSingleDay, function(req, res, next) {
-    res.render('./main_administrator/statistics',{singleDayOrders: req.niz_proracuna_po_danima});
+router.get('/statistics',database.getNumberOfOrdersForSingleDay,
+                              database.getMostPopularCategoryOfArticleForSingleDays,
+                              database.getNumberOfOrdersByDay,
+                              database.getTotalProfitForSingleDay,
+    function(req, res, next) {
+    res.render('./main_administrator/statistics',{
+        singleDayOrders: req.niz_proracuna_po_danima,
+        popularItemCategories: req.niz_najpopularnijih_kategorija,
+        number_of_orders: req.niz_narudzbi_po_danu,
+        profit: req.profit_po_danu
+    });
 });
 
 
