2661af6e58524d2035452df673088d1466b9df27
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-03 18:31:10 +0100

    Implemented list of shops and items that they contain.

---
+++ routes/sales_administrator/shop_items.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+const pg = require("pg");
+const alert = require("alert");
+
+const config = {
+    user: 'vhaxxure',
+    database: 'vhaxxure',
+    password: 'PRTQj-BsWP_lwQCZdqJH94vbpZHUkuAx',
+    host: 'tai.db.elephantsql.com',
+    port: 5432,
+    max: 100,
+    idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+let database={
+    getAllItemsFromYourShops: function(req, res, next){
+        let id_korisnika = req.user.id_korisnika;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select t.id_trgovine,array_agg(a.id_artikla) as id_artikala, array_agg(a.cijena_artikla) as cijena_artikala, array_agg(a.naziv_artikla) as nazivi_artikala,
+                          array_agg(a.dostupna_kolicina) as kolicina_artikala, t.naziv_trgovine
+                          from artikal a, trgovina t, artikal_trgovina at
+                          where a.id_artikla = at.id_artikla
+                          and t.id_trgovine = at.id_trgovine
+                          and t.id_menadzera = $1
+                          group by t.id_trgovine
+                          order by t.id_trgovine`,[id_korisnika],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.artikli_iz_trgovina = result.rows;
+                    next();
+                }
+            });
+        });
+    }
+}
+
+router.get('/', database.getAllItemsFromYourShops,function(req, res, next) {
+    res.render('./sales_administrator/crud_for_items_from_shops',{items_from_shops: req.artikli_iz_trgovina});
+});
+
+
+
+module.exports = router;
