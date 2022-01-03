0c1379fcaa167370ae30de6cd91baff6dbdcee02
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-03 14:16:19 +0100

    Added deleting all orders.

--- routes/sales_administrator/approving_orders.js
+++ routes/sales_administrator/approving_orders.js
@@ -26,7 +26,7 @@ let database={
             if(err)
                 res.end(err);
             client.query(`select k.id_korpe, sum(at.kolicina*a.cijena_artikla) as pojedinacni_racun, ko.ime, ko.prezime,
-                          array_agg(a.naziv_artikla) as artikli, array_agg(t.naziv_trgovine) as prodavnice, k.status, array_agg(n.vrijeme_narudzbe), n.datum_narudzbe
+                          array_agg(a.naziv_artikla) as artikli, array_agg(t.naziv_trgovine) as prodavnice, k.status, array_agg(n.vrijeme_narudzbe) as vrijeme_narudzbi, n.datum_narudzbe
                           from artikal a, artikal_trgovina at, korpa k, artikal_narudzba an, trgovina t, korisnik ko, narudzba n
                           where an.id_artikal_trgovina = at.id
                           and ko.id_korisnika = k.id_kupca
@@ -69,7 +69,7 @@ router.post('/approve/:id', function(req, res, next) {
         if (err)
             throw(err);
         else {
-            client.query(`update narudzba
+            client.query(`update korpa
                           set status = 1
                           where id_korpe = $1`, [req.params.id], function (err, result) {
                 don();
@@ -77,7 +77,7 @@ router.post('/approve/:id', function(req, res, next) {
                     throw(err);
                 else{
                     alert('Successfully accepted order!');
-                    res.redirect('/home/users');
+                    res.redirect('/home/sales_administrator/approving_orders');
                 }
             });
         }
@@ -94,7 +94,7 @@ router.post('/reject/:id', function(req, res, next) {
         if (err)
             throw(err);
         else {
-            client.query(`update narudzba
+            client.query(`update korpa
                           set status = -1
                           where id_korpe = $1`, [req.params.id], function (err, result) {
                 don();
@@ -102,7 +102,31 @@ router.post('/reject/:id', function(req, res, next) {
                     throw(err);
                 else{
                     alert('Successfully rejected order!');
-                    res.redirect('/home/users');
+                    res.redirect('/home/sales_administrator/approving_orders');
+                }
+            });
+        }
+    });
+});
+
+
+router.get('/delete_all',
+    function(req, res, next) {
+        res.redirect('/home/sales_administrator/approving_orders');
+    });
+
+router.post('/delete_all', function(req, res, next) {
+    pool.connect(function (err, client, don) {
+        if (err)
+            throw(err);
+        else {
+            client.query(`delete from korpa k`, function (err, result) {
+                don();
+                if (err)
+                    throw(err);
+                else{
+                    alert('Successfully deleted all orders from your shops!');
+                    res.redirect('/home/sales_administrator/approving_orders');
                 }
             });
         }
