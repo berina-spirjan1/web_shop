67d2d52dd8ae47d8a4d98f2b12cc57d86523fffe
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-04 15:22:05 +0100

    Added functions for most popular, random and items that are in interest of user.

--- routes/customer.js
+++ routes/customer.js
@@ -1,10 +1,103 @@
 const express = require('express');
 const router = express.Router();
 
+const pg = require("pg");
+const alert = require("alert");
 
+const config = {
+    user: 'vhaxxure',
+    database: 'vhaxxure',
+    password: 'PRTQj-BsWP_lwQCZdqJH94vbpZHUkuAx',
+    host: 'tai.db.elephantsql.com',
+    port: 5432,
+    max: 100,
+    idleTimeoutMillis: 30000,
+};
 
-router.get('/', function(req, res, next) {
-    res.render('index',{title:'GOTOVOOO'});
+const pool = new pg.Pool(config);
+
+let database = {
+    getMostPopularItems : function (req, res, next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select y.id_artikla, y.naziv_artikla, y.dostupna_kolicina, y.cijena_artikla, y.opis_artikla, y.sadrzaj_artikla, ((y.suma_ocjene)/max(y.broj_ocjena)) as prosjek
+                          from(select a.id_artikla, a.naziv_artikla, a.dostupna_kolicina, a.cijena_artikla, a.opis_artikla, a.sadrzaj_artikla,
+                               sum(o.ocjena)as suma_ocjene,count(o.ocjena) as broj_ocjena
+                               from artikal a, ocjene_artikala oa, ocjena o
+                               where a.id_artikla = oa.id_artikal
+                               and o.id_ocjena = oa.id_ocjene
+                               group by a.id_artikla, a.naziv_artikla, a.dostupna_kolicina, a.cijena_artikla, a.opis_artikla,
+                               a.sadrzaj_artikla) y
+                          group by y.id_artikla, y.naziv_artikla, y.dostupna_kolicina, y.cijena_artikla, y.opis_artikla,
+                                   y.sadrzaj_artikla, y.suma_ocjene
+                          order by prosjek desc`,function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.najpopularniji_artikli = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getRandomItems: function (req, res, next){
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`SELECT x.id_artikla, x.sadrzaj_artikla, x.naziv_artikla, x.opis_artikla, x.cijena_artikla, x.dostupna_kolicina
+                          FROM artikal x
+                          ORDER BY RANDOM()
+                          LIMIT 10`,function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.nasumicni_artikli = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getItemsFromUserTags: function (req, res, next){
+
+        let id_korisnika = req.user.id_korisnika;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select a.id_artikla, a.cijena_artikla, a.naziv_artikla, a.opis_artikla, a.dostupna_kolicina,a.sadrzaj_artikla
+                          from artikal a, interesi_korisnika ik, kategorija k, kategorija_artikla ak, artikal_tagovi at, artikal_kategorija_artikla aka, trgovina t
+                          where ik.id_kategorija_artikla = ak.id_kategorija_artikla
+                          and at.id_taga = ik.id_tagovi_artikla
+                          and at.id_artikla = a.id_artikla
+                          and ik.id_korisnika = $1
+                          and aka.id_kategorija_artikla = ak.id_kategorija_artikla
+                          and aka.id_artikla = a.id_artikla
+                          and t.id_kategorije = k.id_kategorije
+                          group by a.id_artikla`, [id_korisnika] ,function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.artikli_na_osnovu_interesa = result.rows;
+                    next();
+                }
+            });
+        });
+    }
+}
+
+router.get('/', database.getMostPopularItems,
+                     database.getRandomItems,
+                     database.getItemsFromUserTags,
+    function(req, res, next) {
+    res.render('index',{
+        most_popular: req.najpopularniji_artikli,
+        random: req.nasumicni_artikli,
+        items_from_interest: req.artikli_na_osnovu_interesa
+    });
 });
 
 
