045b5b447bbb3df71fb2567bb0363b0419989bd5
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-04 15:27:31 +0100

    Changed redirect to sales administrator page.

--- routes/main_administrator/home.js
+++ routes/main_administrator/home.js
@@ -283,7 +283,7 @@ router.get('/', function(req, res, next) {
         res.redirect('/home/main_administrator');
     }
     else if(req.user.id_tip_korisnika === 3) {
-        res.redirect('/home/sales_administrator/shops');
+        res.redirect('/home/sales_administrator');
     }
     else {
         res.redirect('/home/customer');
