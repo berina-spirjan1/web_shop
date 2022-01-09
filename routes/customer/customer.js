d8d80913ba27479a77dc42777c3ee0ffcb9ec822
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-09 23:58:14 +0100

    Finished page for items.

--- routes/customer/customer.js
+++ routes/customer/customer.js
@@ -243,6 +243,194 @@ let database = {
                 }
             });
         });
+    },
+    getAllItemsForSingleShop: function(req, res, next){
+        let id_trgovine = req.params.id;
+        let id_kategorije = req.params.id_category;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select distinct a.id_artikla, a.naziv_artikla, f.path, a.cijena_artikla
+                          from trgovina t, artikal a, artikal_trgovina at, fotografija f, kategorija_artikla ka, artikal_kategorija_artikla aka
+                          where a.id_artikla = at.id_artikla
+                          and t.id_trgovine = at.id_trgovine
+                          and f.id_fotografije = a.id_naslovnica
+                          and aka.id_kategorija_artikla = ka.id_kategorija_artikla
+                          and aka.id_artikla = a.id_artikla
+                          and t.id_trgovine = $1
+                          and ka.id_kategorija_artikla = $2
+                          order by id_artikla`,[id_trgovine, id_kategorije],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.artikli_po_kategorijama = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getCoverImageForShop: function(req, res, next){
+        let id_trgovine = req.params.id;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select f.path
+                          from trgovina t, fotografija f
+                          where t.id_pozadina = f.id_fotografije
+                          and t.id_trgovine = $1`,[id_trgovine],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.pozadina_za_prodavnicu = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getDetailInformationsAboutShop: function(req, res, next){
+        let id_trgovine = req.params.id;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select y.naziv_kategorije, y.id_trgovine, y.ime, y.prezime, y.naziv_lanca_trgovina, y.path,
+                                 y.naziv_trgovine, y.logo_kategorije, y.boja_kategorije, y.suma_ocjena, (y.suma_ocjena*1.00)/count(o.id_ocjena) as prosjek
+                          from(select t.naziv_trgovine, t.id_trgovine, k.ime, k.prezime, lt.naziv_lanca_trgovina, f.path,
+                                      kat.naziv_kategorije,kat.logo_kategorije, kat.boja_kategorije, sum(o.ocjena) as suma_ocjena
+                               from trgovina t, lanac_trgovina lt, fotografija f, kategorija kat, korisnik k, ocjene_trgovina ot, ocjena o
+                               where t.id_lanca_trgovina = lt.id_lanca_trgovina
+                               and lt.id_logo = f.id_fotografije
+                               and k.id_korisnika = t.id_menadzera
+                               and kat.id_kategorije = t.id_kategorije
+                               and ot.id_korisnika = k.id_korisnika
+                               and t.id_trgovine = $1
+                               and ot.id_trgovine = t.id_trgovine
+                               and ot.id_ocjene = o.id_ocjena
+                               group by t.naziv_trgovine, t.id_trgovine, k.ime, k.prezime, lt.naziv_lanca_trgovina, f.path,
+                                        kat.naziv_kategorije, kat.logo_kategorije, kat.boja_kategorije) y, ocjena o, trgovina t, ocjene_trgovina ot
+                          where ot.id_trgovine = t.id_trgovine
+                          and o.id_ocjena = ot.id_ocjene
+                          group by y.naziv_kategorije, y.id_trgovine, y.ime, y.prezime, y.naziv_lanca_trgovina, y.path, y.naziv_trgovine,
+                                 y.logo_kategorije, y.boja_kategorije, y.suma_ocjena`,[id_trgovine],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.info_o_trgovini = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getOneImageForItem: function (req, res, next){
+        let id_trgovine = req.params.id;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select a.id_artikla,array_agg(f.path) as slike_artikla
+                          from trgovina t, artikal a, artikal_trgovina at, fotografija f, artikal_fotografija af
+                          where a.id_artikla = at.id_artikla
+                          and t.id_trgovine = at.id_trgovine
+                          and af.id_fotografije = f.id_fotografije
+                          and af.id_artikla = a.id_artikla
+                          and t.id_trgovine = $1
+                          group by a.id_artikla
+                          order by a.id_artikla`,[id_trgovine],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.slike_arikala = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getCategoriesOfItemsForCurrentShop: function (req, res, next){
+        let id_trgovine = req.params.id;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select distinct ka.id_kategorija_artikla, ka.boja_kategorije, ka.logo_kategorije, ka.naziv_kategorije
+                          from kategorija_artikla ka, artikal a, trgovina t, artikal_trgovina at,artikal_kategorija_artikla aka
+                          where aka.id_kategorija_artikla = ka.id_kategorija_artikla
+                          and a.id_artikla = aka.id_artikla
+                          and t.id_trgovine = at.id_trgovine
+                          and t.id_trgovine = $1
+                          and at.id_artikla = a.id_artikla
+                          order by ka.id_kategorija_artikla`,[id_trgovine],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.kategorije_artikala_za_prodavnicu = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getItemsForCurrentCategory:function(req, res, next){
+        let id_trgovine = req.params.id;
+        let id_kategorije = req.params.id_category;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select distinct a.id_artikla, a.naziv_artikla, f.path, a.cijena_artikla
+                          from trgovina t, artikal a, artikal_trgovina at, fotografija f, kategorija_artikla ka, artikal_kategorija_artikla aka
+                          where a.id_artikla = at.id_artikla
+                          and t.id_trgovine = at.id_trgovine
+                          and f.id_fotografije = a.id_naslovnica
+                          and aka.id_kategorija_artikla = ka.id_kategorija_artikla
+                          and aka.id_artikla = a.id_artikla
+                          and t.id_trgovine = $1
+                          and ka.id_kategorija_artikla = $2
+                          order by a.id_artikla`,[id_trgovine, id_kategorije],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.artikli_kategorije = result.rows;
+                    next();
+                }
+            });
+        });
+    },
+    getPictures: function(req, res, next){
+        let id_trgovine = req.params.id;
+        let id_kategorije = req.params.id_category;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query(`select a.id_artikla,array_agg(f.path) as slike_artikla
+                          from trgovina t, artikal a, artikal_trgovina at, fotografija f, artikal_fotografija af,
+                               kategorija_artikla ka, artikal_kategorija_artikla aka
+                          where a.id_artikla = at.id_artikla
+                          and t.id_trgovine = at.id_trgovine
+                          and aka.id_artikla = a.id_artikla
+                          and aka.id_kategorija_artikla = ka.id_kategorija_artikla
+                          and af.id_fotografije = f.id_fotografije
+                          and af.id_artikla = a.id_artikla
+                          and t.id_trgovine = $1
+                          and ka.id_kategorija_artikla = $2
+                          group by a.id_artikla
+                          order by a.id_artikla`,[id_trgovine, id_kategorije],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.slike = result.rows;
+                    next();
+                }
+            });
+        });
     }
 }
 
@@ -307,7 +495,6 @@ router.get('/single_item/:id/shops',database.getInfoAboutSingleItem,
     database.getMarkOfItem,
     database.getCoverImageForShops,
     function(req, res, next){
-    console.info("ISIPISI",req.pozadine_prodavnica[0])
         res.render('./customers/single_item_page_shops',{
             single_item: req.informacije_o_artiklu,
             cover_image: req.pozadina,
@@ -327,7 +514,63 @@ router.get('/single_item/:id/images',database.getInfoAboutSingleItem,
         });
 });
 
+router.get('/single_shop/:id', database.getAllItemsForSingleShop,
+                                    database.getCoverImageForShop,
+                                    database.getDetailInformationsAboutShop,
+    function(req, res, next){
 
+
+        res.render('./customers/single_shop_page',{
+            items: req.niz_artikala,
+            cover: req.pozadina_za_prodavnicu,
+            info: req.info_o_trgovini,
+            mark_of_item: Math.round(req.info_o_trgovini[0].prosjek,2)
+        });
+});
+
+router.get('/single_shop/:id/items', database.getAllItemsForSingleShop,
+    database.getCoverImageForShop,
+    database.getDetailInformationsAboutShop,
+    database.getOneImageForItem,
+    function(req, res, next){
+        res.render('./customers/single_shop_page_items',{
+            items: req.niz_artikala,
+            cover: req.pozadina_za_prodavnicu,
+            info: req.info_o_trgovini,
+            images: req.slike_arikala
+        });
+});
+
+router.get('/single_shop/:id/categories',
+    database.getAllItemsForSingleShop,
+    database.getCoverImageForShop,
+    database.getDetailInformationsAboutShop,
+    database.getCategoriesOfItemsForCurrentShop,
+    function(req, res, next){
+        res.render('./customers/single_shop_page_categories',{
+            items: req.niz_artikala,
+            cover: req.pozadina_za_prodavnicu,
+            info: req.info_o_trgovini,
+            categories: req.kategorije_artikala_za_prodavnicu
+        });
+});
+
+router.get('/single_shop/:id/categories/:id_category',
+    database.getAllItemsForSingleShop,
+    database.getCoverImageForShop,
+    database.getDetailInformationsAboutShop,
+    database.getCategoriesOfItemsForCurrentShop,
+    database.getPictures,
+    database.getItemsForCurrentCategory,
+    function(req, res, next){
+        res.render('./customers/single_shop_page_categories_items',{
+            items: req.artikli_kategorije,
+            cover: req.pozadina_za_prodavnicu,
+            info: req.info_o_trgovini,
+            images: req.slike,
+            categories: req.kategorije_artikala_za_prodavnicu
+        });
+    });
 
 
 module.exports = router;
