3613db67dbb412b056cbc19f4e174400a8becc83
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-17 15:37:52 +0100

    Updated login for saving in cookie user informations.

--- routes/main_administrator/login.js
+++ routes/main_administrator/login.js
@@ -1,12 +1,49 @@
 const express = require('express');
-const router = express.Router();
 const passport = require('passport');
+const pg = require("pg");
+const router = express.Router();
+const saltRounds = 10;
+let alert = require('alert');
 
-router.get('/', function(req, res, next) {
+const config = {
+    user: 'vhaxxure',
+    database: 'vhaxxure',
+    password: 'PRTQj-BsWP_lwQCZdqJH94vbpZHUkuAx',
+    host: 'tai.db.elephantsql.com',
+    port: 5432,
+    max: 100,
+    idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+let database = {
+    getAllInformationAboutCurrentUser: function(req,res,next){
+        let username = req.body.username;
+
+        pool.connect(function (err,client,done) {
+            if(err)
+                res.end(err);
+            client.query("select * from korisnik where username=$1",[username],function (err,result) {
+                done();
+                if(err)
+                    res.sendStatus(500);
+                else{
+                    req.podaci = result.rows;
+                    res.cookie('korisnik',req.podaci);
+
+                    next();
+                }
+            });
+        });
+    }
+}
+
+router.get('/',function(req, res, next) {
     res.render('./registration/login' );
 });
 
-router.post('/', (req,res,next) =>{
+router.post('/', database.getAllInformationAboutCurrentUser,(req,res,next) =>{
     if (!req.body.username || !req.body.password) {
         console.info("Nisu podaci popunjeni");
         req.flash('error', 'Molim Vas popunite sva polja.');
