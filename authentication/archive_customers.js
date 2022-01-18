125f430c086c81205bf3ee50a6b5a90daa490768
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2022-01-18 02:49:16 +0100

    Added authentification for archive users.

--- authentication/archive_customers.js
+++ authentication/archive_customers.js
@@ -1,7 +1,7 @@
 module.exports = {
-    ensureAuthenticatedCustomer: function (req,res,next) {
+    ensureAuthenticatedArchiveCustomer: function (req,res,next) {
         if(req.isAuthenticated()){
-            if(req.user.id_tipa_korisnika === 2 && (req.user.status!=='blokiran' || req.user.status!=='blokiran na 15'))
+            if(req.user.id_tipa_korisnika === 2 && req.user.status!=='arhiviran')
                 return next();
         }
         req.flash('error','You need to login to be able to continue using app.');
